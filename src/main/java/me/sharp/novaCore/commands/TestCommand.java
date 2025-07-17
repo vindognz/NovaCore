ba04a6f7e76a427b5944b72b6530cc6d3e4021fc
Vincent Rodley <vindognz@gmail.com>
2025-07-18 10:14:56 +1200

    added rings parameter and added a delay between rings

--- src/main/java/me/sharp/novaCore/commands/TestCommand.java
+++ src/main/java/me/sharp/novaCore/commands/TestCommand.java
@@ -7,15 +7,35 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class TestCommand implements CommandExecutor {
 
-    public void ParticleCircle(Player player, Particle type, Double rad, Integer particle_count) {
+    private final JavaPlugin plugin;
+
+    public  TestCommand(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void ParticleCircle(Player player, Particle type, double rad, int particle_count, int rings) {
         Location base = player.getLocation();
-        for (int i = 0; i <= particle_count; i++) {
-            Double angle = (2*Math.PI / particle_count) * i;
-            Location location = base.clone().add(rad*Math.cos(angle), 0, rad*Math.sin(angle));
-            player.getWorld().spawnParticle(type,location,1, 0, 0, 0, 0);
+        double betweenRings = rad / rings;
+
+        for (int ring_index = 0; ring_index < rings; ring_index++) {
+            double ring_radius = betweenRings * (ring_index + 1); // Still avoid zero-radius
+            int delay_ticks = ring_index * 3; // First ring: 0 ticks, next: 10, 20, ...
+
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    for (int particle_index = 0; particle_index <= particle_count; particle_index++) {
+                        double angle = (2 * Math.PI / particle_count) * particle_index;
+                        Location location = base.clone().add(ring_radius * Math.cos(angle), 0, ring_radius * Math.sin(angle));
+                        player.getWorld().spawnParticle(type, location, 1, 0, 0, 0, 0);
+                    }
+                }
+            }.runTaskLater(plugin, delay_ticks);
         }
     }
 
@@ -29,7 +49,7 @@ public class TestCommand implements CommandExecutor {
         Player player = (Player) sender;
         if (command.getName().equalsIgnoreCase("test")) {
             player.sendMessage("Test!");
-            ParticleCircle(player,Particle.WHITE_SMOKE,2.5,16);
+            ParticleCircle(player,Particle.WHITE_SMOKE,2.5,16, 3);
         }
         return true;
     }
